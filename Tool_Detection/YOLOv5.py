11fb1924ccf9826f63bb696560eca066bdbbef73
RebeccaHisey <rebecca.hisey@gmail.com>
2024-04-11 18:22:38 -0400

    Updated tool detection baseline for 2024

---
+++ Tool_Detection/YOLOv5.py
@@ -0,0 +1,44 @@
+import numpy
+import os
+import cv2
+import yaml
+from ultralytics import YOLO
+
+class YOLOv5:
+    def __init__(self):
+        self.model = None
+        self.class_mapping = None
+
+    def loadModel(self,modelFolder,modelName=None):
+        self.model = YOLO(os.path.join(modelFolder,"train/weights/best.pt"))
+        with open(os.path.join(modelFolder,"config.yaml"),"r") as f:
+            config = yaml.safe_load(f)
+            self.class_mapping = config['class_mapping']
+
+    def predict(self,image):
+        results = self.model.predict(image)
+        bboxes = results[0].boxes.xyxy.cpu().numpy()
+        class_nums = results[0].boxes.cls.cpu().numpy()
+        confs = results[0].boxes.conf.cpu().numpy()
+        resultList = []
+        print(bboxes.shape)
+        for i in range(bboxes.shape[0]):
+            class_value = class_nums[i]
+            class_name = self.class_mapping[class_value]
+            xmin,ymin,xmax,ymax = bboxes[i]
+            confidence = confs[i] #[class_value]
+            bbox = {"class":class_name,
+                    "xmin":round(xmin),
+                    "ymin":round(ymin),
+                    "xmax":round(xmax),
+                    "ymax":round(ymax),
+                    "conf":confidence}
+            resultList.append(bbox)
+        return str(resultList)
+
+    def createModel(self,config=None):
+        if config is None:
+            self.model = YOLO("yolov5su.pt")
+        else:
+            self.model = YOLO(config)
+        return self.model
